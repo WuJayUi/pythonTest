b082cc7e632874694ce757b6461a223dbcc77d4d
Jay <famousoneman@gmail.com>
2023-05-26 13:33:03 +0800

    fix

--- flask-heroku/app.py
+++ flask-heroku/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__) # __name__ 代表目前執行的模組
 
 @app.route("/") #涵式的裝飾(Decorator) 以涵式為基礎 提供附加的功能
 def home():
-    return "Hello Flask   Go Go PowerRanger"
+    return "狗狗睪丸被割  Go Go PowerRanger"
 
 @app.route("/test") #代表要處理的網路路徑 /後面為路徑
 def test():
